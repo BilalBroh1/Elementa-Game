23a06d9e9acf30d86977e2c033d53b18c0bcbc45
Anas Jamil <146596074+Anas-Jamil@users.noreply.github.com>
2025-04-21 19:07:13 -0400

    main template

---
+++ Elementa-Game/src/main.py
@@ -0,0 +1,10 @@
+
+#def main():
+    # Create an instance of the Library class
+    #library = Library()
+
+    # Start the application by calling the login method
+    #library.login()
+
+#if __name__ == "__main__":
+    #main()
